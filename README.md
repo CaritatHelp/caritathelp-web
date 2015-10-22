a684fec128c31e3aa96662ed73052fa109d4aaf4
ChypRiotE <nicolastem@hotmail.fr>
2015-10-22 20:47:17 +0800

    mise en place dev

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # caritathelp-web
 Test avec jerem
 CA marche très bien mon coco !
+
+
+===
+##Couleurs
+Vert navbar: #93c47d
+Vert bloc: #b6d7a8
+
+##Polices:
+Titres: Roboto
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,22 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     rename = require('gulp-rename');
 var less = require('gulp-less');
+var browserSync = require('browser-sync');
 
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
 
 gulp.task('styles', function(){
-  gulp.src(['less/**/*.less'])
+  gulp.src(['less/main.less'])
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
@@ -13,9 +25,11 @@ gulp.task('styles', function(){
     }}))
     .pipe(less())
     .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({stream:true}))
 });
 
 
-gulp.task('default', function(){
+gulp.task('default', ['browser-sync'], function(){
   gulp.watch("less/**/*.less", ['styles']);
+  gulp.watch("*.html", ['bs-reload']);
 });

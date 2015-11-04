bb7a61a72299f51218552a66d81f36f5b4e35925
ChypRiotE <nicolastem@hotmail.fr>
2015-11-05 02:20:14 +0800

    restructuration des dossiers, nouveau gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     rename = require('gulp-rename'),
-    changed = require('gulp-changed');
-var less = require('gulp-less');
-var jade = require('gulp-jade');
-var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var browserSync = require('browser-sync');
+    changed = require('gulp-changed'),
+    jade = require('gulp-jade'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    runSequence = require('run-sequence'),
+    browserSync = require('browser-sync');
 
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-       baseDir: "./"
+       baseDir: "./public"
     }
   });
 });
@@ -21,15 +22,15 @@ gulp.task('bs-reload', function () {
 });
 
 gulp.task('jade', function(){
-  gulp.src(['jade/*.jade'])
-    .pipe(changed('view/', {extension: '.html'}))
+  gulp.src(['src/jade/*.jade'])
+    .pipe(changed('public/view/', {extension: '.html'}))
     .pipe(jade())
-    .pipe(gulp.dest('view/'))
+    .pipe(gulp.dest('public/view/'))
     .pipe(browserSync.reload({stream:true}))
 });
 
 gulp.task('styles', function(){
-  gulp.src(['less/main.less'])
+  gulp.src(['src/less/main.less'])
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
@@ -39,14 +40,40 @@ gulp.task('styles', function(){
     .pipe(autoprefixer('last 2 versions'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('public/css/'))
     .pipe(browserSync.reload({stream:true}))
 });
 
+gulp.task('scripts', function(){
+  gulp.src(['src/js/**/*.js'])
+    .pipe(changed('public/js/'))
+    .pipe(gulp.dest('public/js/'))
+    .pipe(browserSync.reload({stream:true}))
+});
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("less/**/*.less", ['styles']);
-  gulp.watch("jade/**/*.jade", ['jade']);
-  gulp.watch("js/**/*.js", ['bs-reload']);
-  gulp.watch("**/*.html", ['bs-reload']);
+gulp.task('copyfiles', function(){
+  gulp.src(['src/fonts/*'])
+    .pipe(changed('public/fonts/**/*'))
+    .pipe(gulp.dest('public/fonts'))
+  gulp.src(['src/img/**/*'])
+    .pipe(changed('public/img/'))
+    .pipe(gulp.dest('public/img'))
+  gulp.src(['src/**/*.html'])
+    .pipe(changed('public/**/*.html'))
+    .pipe(gulp.dest('public/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('build', function() {
+  runSequence(['styles', 'scripts', 'jade', 'copyfiles']);
+});
+
+gulp.task('watch', function(){
+  gulp.watch("src/less/**/*.less", ['styles']);
+  gulp.watch("src/jade/**/*.jade", ['jade']);
+  gulp.watch("src/js/**/*.js", ['scripts']);
+  gulp.watch(["src/**/*.html", "src/fonts/*", "src/img/*"], ['copyfiles']);
+});
+
+gulp.task('default', ['build', 'browser-sync', 'watch'], function(){
 });

fc18f0008a92d30dda78a4882d5a1f5dff5593f2
ChypRiotE <nicolastem@hotmail.fr>
2015-11-11 20:08:17 +0800

    edits travis

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function(config) {
 
     files: [
     	'node_modules/angular/angular.js',
+    	'node_modules/angular-route/angular-route.js',
     	'node_modules/angular-mocks/angular-mocks.js',
       'src/js/app.js',
       'src/js/**/*.js',

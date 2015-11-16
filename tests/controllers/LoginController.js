356567c17c2e77112a818ba00e6e761944a65684
ChypRiotE <nicolastem@hotmail.fr>
2015-11-16 19:04:22 +0800

    debut tests fail

--- tests/controllers/LoginController.js
+++ tests/controllers/LoginController.js
@@ -16,4 +16,14 @@ describe('LoginController :', function() {
   		expect(controller.error).toEqual(true);
   	});
   });
+
+  describe('[Test empty values] - ', function() {
+    it('should display an error if  are empty', function() {
+      var controller = $controller('LoginController', {});
+      controller.mail = "root@root.com";
+      controller.password = "less8";
+      controller.SignIn();
+      expect(controller.error).toEqual(true);
+    });
+  });
 });

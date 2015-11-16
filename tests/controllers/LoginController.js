c7de5bd8b6316e653b1b0c4994852723bbd029f2
ChypRiotE <nicolastem@hotmail.fr>
2015-11-16 20:06:06 +0800

    ajout de tests

--- tests/controllers/LoginController.js
+++ tests/controllers/LoginController.js
@@ -4,26 +4,38 @@ describe('LoginController :', function() {
   var $controller;
 
   beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
+    controller = _$controller_('LoginController', {});
   }));
 
-  describe('[Test empty values] - ', function() {
+  describe('[Test components] ', function() {
+    it('should have SignIn function ', function () {
+      expect(typeof(controller.SignIn)).toEqual('function');
+    });
+    
+    it('should have SignOut function', function() {
+      expect(typeof(controller.SignOut)).toEqual('function');
+    });
+  });
+
+  describe('[Test fields] ', function() {
   	it('should display an error if fields are empty', function() {
-      var controller = $controller('LoginController', {});
       controller.mail = "";
       controller.password = "";
       controller.SignIn();
   		expect(controller.error).toEqual(true);
   	});
-  });
-
-  describe('[Test empty values] - ', function() {
-    it('should display an error if  are empty', function() {
-      var controller = $controller('LoginController', {});
+    it('should display an error if password is less than 8 characters', function() {
       controller.mail = "root@root.com";
       controller.password = "less8";
       controller.SignIn();
       expect(controller.error).toEqual(true);
     });
+    it('should display an error if password doesn\'t have a number', function() {
+      controller.mail = "root@root.com";
+      controller.password = "passwordwithoutanumber";
+      controller.SignIn();
+      expect(controller.error).toEqual(true);
+    });
   });
+
 });

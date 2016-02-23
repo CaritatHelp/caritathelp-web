573d624adf06cd1db12ff35cb9dec73ba62d1438
ChypRiotE <nicolastem@hotmail.fr>
2016-02-23 12:08:29 +0800

    settings controller

--- src/js/settings/settingsController.js
+++ src/js/settings/settingsController.js
@@ -1,7 +1,18 @@
 'use strict';
 module.exports = /*@ngInject*/ function (dataService, userService) {
-	var _this = this;
+	var vm = this;
 	var usc = userService;
 
-	_this.user = usc.user();
+	vm.user = usc.user();
+	vm.connected = usc.connected();
+
+	vm.tab = 1;
+
+	this.setTab = function (activeTab) {
+		this.tab = activeTab;
+	};
+	this.isSet = function (tab) {
+		return this.tab === tab;
+	};
+
 };

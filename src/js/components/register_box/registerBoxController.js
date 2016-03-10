67e51153e8a729afdded6154e04788ad5ffc24bb
ChypRiotE <nicolastem@hotmail.fr>
2016-03-11 04:31:41 +0800

    clean up less files

--- src/js/components/register_box/registerBoxController.js
+++ src/js/components/register_box/registerBoxController.js
@@ -10,44 +10,72 @@ module.exports = /*@ngInject*/ function (dataService, userService, $location) {
 	_this.error = false;
 	_this.errorMessage = '';
 
-	function	verif() {
-		if (_this.mail === undefined || _this.mail === '') {
-			return false;
+	function verif() {
+		var result = false;
+		if (!/[a-zA-Z]/.test(_this.password)) {
+			_this.errorMessage += 'Votre mot de passe doit contenir au moins une lettre.<br/>';
+			result = true;
 		}
-		if (_this.password === undefined || _this.password === '') {
-			return false;
+		if (!/\d/.test(_this.password)) {
+			_this.errorMessage += 'Votre mot de passe doit contenir au moins un chiffre.<br/>';
+			result = true;
 		}
-		return true;
+		if (_this.password === undefined || _this.password.length < 8) {
+			_this.errorMessage += 'Votre mot de passe doit faire au moins 8 caracteres.<br/>';
+			result = true;
+		}
+		if (_this.password !== _this.confirmPassword) {
+			_this.errorMessage += 'Les mots de passe ne correspondent pas.<br/>';
+			result = true;
+		}
+		return result;
+	}
+	function	cleanup() {
+		_this.gender = _this.gender === 'homme' ? 'm' : 'f';
+		_this.birthday = String(_this.year) + '-' + _this.month + '-' + _this.day;
+	}
+	function	reset() {
+		_this.error = false;
+		_this.errorMessage = '';
 	}
 
-	_this.login = function () {
-		if (!verif()) {
+	this.register = function () {
+		reset();
+		cleanup();
+		if (verif()) {
 			_this.error = true;
-			_this.errorMessage = 'Veuillez entrer votre addresse mail et votre mot de passe.';
 			return;
 		}
 
-		angular.element('#buttonConnect').prepend('<i class="fa fa-spin fa-spinner"></i> ').attr('disabled', true);
-		//Requete de login
-		dsc.login(_this.mail, _this.password)
+		angular.element('#buttonRegister').prepend('<i class="fa fa-spin fa-spinner"></i> ').attr('disabled', true);
+
+		//Register request
+		dsc.register(_this.mail, _this.password, _this.firstname, _this.lastname, _this.birthday, _this.gender)
 			.success(function (data) {
 				if (data.status === 200) {
-					//On remplit le service USC
 					usc.connect(data.response);
-					//redirection vers la page d'accueil
 					$location.path('/home');
 				} else {
-					//Mdp ou email invalide
-					_this.error = 'true';
-					_this.errorMessage = 'Votre compte n\'a pas été reconnu. Veuillez vérifier vos informations';
+					//Erreur serveur
+					_this.error = true;
+					_this.errorMessage = data.message;
 				}
 			})
 			.error(function () {
 				//erreur client
 				_this.error = 'true';
 				_this.errorMessage = 'Impossible de joindre le serveur, veuillez réessayer dans quelques minutes.';
-			}).finally(function () {
-				angular.element('#buttonConnect').html('Connexion').attr('disabled', false);
+			})
+			.finally(function () {
+				angular.element('#buttonRegister').html('S\'enregister').attr('disabled', false);
 			});
+	};
+	this.checkBirthdate = function (rf) {
+		if (rf.$submitted || (rf.selectDay.$touched && rf.selectMonth.$touched && rf.selectYear.$touched)) {
+			if (rf.selectDay.$invalid || rf.selectMonth.$invalid || rf.selectYear.$invalid) {
+				return true;
+			}
+		}
+		return false;
 	};
 };

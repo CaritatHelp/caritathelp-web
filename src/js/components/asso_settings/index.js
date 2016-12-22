c41f7157d268c19e6f5c076c21f8fbfe7f0a0895
ChypRiotE <nicolastem@hotmail.fr>
2016-12-22 17:26:21 +0100

    asso settings

--- src/js/components/asso_settings/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var app = require('angular').module('caritathelp');
-
-app.directive('assoSettings', require('./assoSettings'));
-app.controller('assoSettingsController', require('./assoSettingsController'));
